b6cf27b3ed0cc06af532f5c9f7b5db2d6c57bf01
Apostol Bakalov <apogza@yahoo.com>
2019-10-02 23:55:41 +0300

    Correct Render method signature

--- PdfViewer/PdfDocumentExtensions.cs
+++ PdfViewer/PdfDocumentExtensions.cs
@@ -12,16 +12,20 @@ namespace PdfViewer
 {
     public static class PdfDocumentExtensions
     {
-        public static WriteableBitmap Render(this PdfDocument document, int page, int width, int height)
+        public static WriteableBitmap Render(this PdfDocument document, int pageIdx, int xDpi, int yDpi)
         {
             WriteableBitmap image = null;
+            PdfPage page = document.Pages[pageIdx];
+            int width = (int)page.Width;
+            int height = (int)page.Height;
+
             using (var pdfBitMap = new PdfBitmap(width, height, true))
             {
                 document.Pages[0].Render(pdfBitMap,
                     0, 0, width, height,
                     Patagames.Pdf.Enums.PageRotate.Normal, Patagames.Pdf.Enums.RenderFlags.FPDF_NONE);
 
-                image = new WriteableBitmap(pdfBitMap.Width, pdfBitMap.Height, 300, 300, PixelFormats.Bgra32, null);
+                image = new WriteableBitmap(pdfBitMap.Width, pdfBitMap.Height, xDpi, yDpi, PixelFormats.Bgra32, null);
 
                 image.WritePixels(new Int32Rect(0, 0, pdfBitMap.Width, pdfBitMap.Height),
                     pdfBitMap.Buffer, pdfBitMap.Stride * pdfBitMap.Height, pdfBitMap.Stride, 0, 0);
